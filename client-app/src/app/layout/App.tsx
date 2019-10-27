2289a243fe2fe7b9b4a14ef564f538f5275f4ef9
Juwami <sean.nguyenf@yahoo.com>
2019-10-26 21:13:58 -0400

    Styling React Components

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
-import { Header, Icon, List } from 'semantic-ui-react';
+import { Header, Icon, List, Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 import NavBar from '../../features/nav/NavBar';
 
@@ -10,15 +10,17 @@ const App = () => {
 	useEffect(() => {
 		axios.get<IActivity[]>('http://localhost:5000/api/activities').then((response) => {
 			// console.log(response);
-			setActivities(response.data)
+			setActivities(response.data);
 		});
 	}, []);
 
 	return (
-		<div>
+		<Fragment>
 			<NavBar />
-			<List>{activities.map((activity) => <List.Item key={activity.id}>{activity.title}</List.Item>)}</List>
-		</div>
+			<Container style={{marginTop: '7em'}}>
+				<List>{activities.map((activity) => <List.Item key={activity.id}>{activity.title}</List.Item>)}</List>
+			</Container>
+		</Fragment>
 	);
 };
 
