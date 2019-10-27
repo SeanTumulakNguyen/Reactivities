999f1d5982f354ef0b7a39be441ffbfc0fc3594f
Juwami <sean.nguyenf@yahoo.com>
2019-10-27 19:26:09 -0400

    Section 5.64

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,19 +1,45 @@
-import React from 'react'
-import { Form, Segment } from 'semantic-ui-react'
+import React, { useState } from 'react';
+import { Form, Segment, Button } from 'semantic-ui-react';
+import { IActivity } from '../../../app/models/activity';
 
-const ActivityForm = () => {
-    return (
-        <Segment>
-            <Form>
-                <Form.Input placeholder='Title'/>
-                <Form.TextArea row={2} placeholder='Description'/>
-                <Form.Input placeholder='Category'/>
-                <Form.Input type='date' placeholder='Date'/>
-                <Form.Input placeholder='City'/>
-                <Form.Input placeholder='Venue'/>
-            </Form>
-        </Segment>
-    )
+interface IProps {
+	setEditMode: (editMode: boolean) => void;
+	activity: IActivity;
 }
 
-export default ActivityForm
+const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: initialFormState }) => {
+	const initializeForm = () => {
+		if (initialFormState) {
+			return initialFormState;
+		} else {
+			return {
+                id: '',
+				title: '',
+				category: '',
+				description: '',
+				date: '',
+				city: '',
+				venue: ''
+			};
+		}
+    };
+    
+    const [activity, setActivity] = useState<IActivity>(initializeForm)
+
+	return (
+		<Segment clearing>
+			<Form>
+				<Form.Input placeholder="Title" value={activity.title}/>
+				<Form.TextArea row={2} placeholder="Description" value={activity.description}/>
+				<Form.Input placeholder="Category" value={activity.category}/>
+				<Form.Input type="date" placeholder="Date" value={activity.date}/>
+				<Form.Input placeholder="City" value={activity.city}/>
+				<Form.Input placeholder="Venue" value={activity.venue}/>
+				<Button floated="right" positive type="submit" content="submit" />
+				<Button onClick={() => setEditMode(false)} floated="right" type="button" content="cancel" />
+			</Form>
+		</Segment>
+	);
+};
+
+export default ActivityForm;
