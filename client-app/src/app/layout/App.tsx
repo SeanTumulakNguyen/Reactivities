4ab597ac1c9aa17c5240d3353b96e3743b763aa2
Juwami <sean.nguyenf@yahoo.com>
2019-11-03 23:08:40 -0500

    finish CRUD methods for database

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -12,35 +12,40 @@ const App = () => {
 
 	const handleSelectActivity = (id: string) => {
 		setSelectedActivity(activities.filter((a) => a.id === id)[0]);
-		setEditMode(false)
+		setEditMode(false);
 	};
 	const handleOpenCreateForm = () => {
 		setSelectedActivity(null);
 		setEditMode(true);
 	};
 	const handleCreateActivity = (activity: IActivity) => {
-		setActivities([ ...activities, activity ]);
-		setSelectedActivity(activity);
-		setEditMode(false);
+		agent.Activities.create(activity).then(() => {
+			setActivities([ ...activities, activity ]);
+			setSelectedActivity(activity);
+			setEditMode(false);
+		});
 	};
 	const handleEditActivity = (activity: IActivity) => {
-		setActivities([ ...activities.filter((a) => a.id !== activity.id), activity ]);
-		setSelectedActivity(activity);
-		setEditMode(false);
+		agent.Activities.update(activity).then(() => {
+			setActivities([ ...activities.filter((a) => a.id !== activity.id), activity ]);
+			setSelectedActivity(activity);
+			setEditMode(false);
+		});
 	};
 	const handleDeleteActivity = (id: string) => {
-		setActivities([...activities.filter(a => a.id !== id)])
-	}
+		agent.Activities.delete(id).then(() => {
+			setActivities([ ...activities.filter((a) => a.id !== id) ]);
+		});
+	};
 
 	useEffect(() => {
-		agent.Activities.list()
-		.then((response) => {
+		agent.Activities.list().then((response) => {
 			// console.log(response);
 			let activities: IActivity[] = [];
 			response.forEach((activity) => {
 				activity.date = activity.date.split('.')[0];
-				activities.push(activity)
-			})
+				activities.push(activity);
+			});
 			setActivities(activities);
 		});
 	}, []);
