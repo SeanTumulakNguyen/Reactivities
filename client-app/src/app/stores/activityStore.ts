a83a76bb2789a8634689bfc486a47ca64381c911
Juwami <sean.nguyenf@yahoo.com>
2019-12-01 21:04:42 -0500

    styling the activity list

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -14,7 +14,12 @@ class ActivityStore {
 
 	@computed
 	get activitiesByDate() {
-		return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+		return this.groupdActivitiesByDate(Array.from(this.activityRegistry.values()));
+	}
+
+	groupdActivitiesByDate(activities: IActivity[]) {
+		const sortedActivities = activities.sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+		return sortedActivities;
 	}
 
 	@action
@@ -29,6 +34,7 @@ class ActivityStore {
 				});
 				this.loadingInitial = false;
 			});
+			console.log(this.groupdActivitiesByDate(activities));
 		} catch (error) {
 			runInAction('load activities error', () => {
 				this.loadingInitial = false;
