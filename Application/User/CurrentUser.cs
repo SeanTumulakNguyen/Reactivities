716294111551e497e1a6f7a89cc2ffc6c89cbc67
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-01-19 10:18:39 -0500

    end of section 12

--- Application/User/CurrentUser.cs
+++ Application/User/CurrentUser.cs
@@ -27,7 +27,14 @@ namespace Application.User
 
             public async Task<User> Handle(Query request, CancellationToken cancellationToken)
             {
-                // handler logic goes here
+                var user = await _userManager.FindByNameAsync(_userAccessor.GetCurrentUsername());
+
+                return new User {
+                    DisplayName= user.DisplayName,
+                    Username = user.UserName,
+                    Token = _jwtGenerator.CreateToken(user),
+                    Image = null
+                };
             }
         }
     }
