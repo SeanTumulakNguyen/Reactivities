507a89ca9cf2db0acbba2ec28161dab7feab4c0b
Sean Nguyen <sean.nguyenf@yahoo.com>
2021-02-03 21:13:17 -0500

    browserRouter and routes

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,6 +5,9 @@ import { ActivityDashboard } from "../../features/activities/dashboard/ActivityD
 import { LoadingComponent } from "./LoadingComponent";
 import ActivityStore from "../stores/activityStore";
 import { observer } from "mobx-react-lite";
+import { Route } from "react-router-dom";
+import { HomePage } from "../../features/home/HomePage";
+import { ActivityForm } from "../../features/activities/form/ActivityForm";
 
 const App = () => {
   const activityStore = useContext(ActivityStore);
@@ -22,7 +25,9 @@ const App = () => {
       <Icon className="users" />
       <Header.Content>Reactivities</Header.Content>
       <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/activities" component={ActivityDashboard} />
+        <Route path="/createActivity" component={ActivityForm} />
       </Container>
     </Fragment>
   );
