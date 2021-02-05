d6fe81d0b62535eac0d599ab4d60e074e2665e59
Sean Nguyen <sean.nguyenf@yahoo.com>
2021-02-04 23:11:31 -0500

    full uncontrolled component with key

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,12 +5,12 @@ import { ActivityDashboard } from "../../features/activities/dashboard/ActivityD
 import { LoadingComponent } from "./LoadingComponent";
 import ActivityStore from "../stores/activityStore";
 import { observer } from "mobx-react-lite";
-import { Route } from "react-router-dom";
+import { Route, RouteComponentProps, withRouter } from "react-router-dom";
 import { HomePage } from "../../features/home/HomePage";
 import { ActivityForm } from "../../features/activities/form/ActivityForm";
 import { ActivityDetails } from "../../features/activities/details/ActivityDetails";
 
-const App = () => {
+const App: React.FC<RouteComponentProps> = ({ location }) => {
   const activityStore = useContext(ActivityStore);
 
   useEffect(() => {
@@ -30,6 +30,7 @@ const App = () => {
         <Route exact path="/activities" component={ActivityDashboard} />
         <Route path="/activities/:id" component={ActivityDetails} />
         <Route
+          key={location.key}
           path={["/createActivity", "/manage/:id"]}
           component={ActivityForm}
         />
@@ -38,4 +39,4 @@ const App = () => {
   );
 };
 
-export default observer(App);
+export default withRouter(observer(App));
